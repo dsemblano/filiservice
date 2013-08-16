2369b04315c2040c2210cbfcf448dfe792eaf3bf
Daniel Semblano <dsemblano@gmail.com>
2013-08-16 04:12:41 -0300

    favicon e apple touhc icon

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico" />
+<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon.png">
 
 <?php wp_head(); ?>
 </head>
