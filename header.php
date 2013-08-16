6314168deec1e622e3a544cd3b34e3ba42533f84
Daniel Semblano <dsemblano@gmail.com>
2013-08-16 04:24:31 -0300

    correcoes SEO

--- header.php
+++ header.php
@@ -11,7 +11,11 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title>
+<?php 
+	if( is_404() ) echo 'P&aacute;gina n&atilde;o encontrada | ' . get_bloginfo('name');
+	else wp_title( '|', true, 'right' );
+?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico" />
